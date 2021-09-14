dd0a8367550530dee579ddaa63c293f438a2991e
Diego Uchoa Neves <diego.livro@gmail.com>
2021-09-14 18:01:21 -0300

    First commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,20 @@
+insert into Expense (id, name, paymentmethod, amount)
+ values (nextval('hibernate_sequence'), 'Groceries', '0','150.50');
+insert into Expense (id, name, paymentmethod, amount)
+ values (nextval('hibernate_sequence'), 'Civilization VI', '1','25.00');
+insert into Expense (id, name, paymentmethod, amount)
+ values (nextval('hibernate_sequence'), 'House', '0','350000.00');
+insert into Expense (id, name, paymentmethod, amount)
+ values (nextval('hibernate_sequence'), 'New Cup', '2','13.00');
+ insert into Expense (id, name, paymentmethod, amount)
+ values (nextval('hibernate_sequence'), 'Notebook', '1','850.00');
+insert into Expense (id, name, paymentmethod, amount)
+ values (nextval('hibernate_sequence'), 'Car wash', '0','35.00');
+ insert into Expense (id, name, paymentmethod, amount)
+ values (nextval('hibernate_sequence'), 'Head Phones', '2','135.50');
+insert into Expense (id, name, paymentmethod, amount)
+ values (nextval('hibernate_sequence'), 'Smart Watch', '1','155.50');
+ insert into Expense (id, name, paymentmethod, amount)
+ values (nextval('hibernate_sequence'), 'Kindle', '0','299.90');
+insert into Expense (id, name, paymentmethod, amount)
+ values (nextval('hibernate_sequence'), 'Printer', '2','430.00');
